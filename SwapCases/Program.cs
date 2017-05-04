da4dc7d7cb140d86a4303e044eca62b60e2b7515
Carlos Hopkins <Carlos Hopkins>
2017-05-04 18:50:44 -0400

    Add project files.

---
+++ SwapCases/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlternateCase("HeLLLLooooo"));
+            
+        }
+        public static string AlternateCase(string s)
+        {
+            var arr = s.ToCharArray();
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(arr[i]))
+                    arr[i] = char.ToLower(arr[i]);
+                else
+                    arr[i] = char.ToUpper(arr[i]);
+            }
+            string result = new string(arr);
+            return result;
+        }
+    }
+}
